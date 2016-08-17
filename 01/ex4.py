63a17f7656d0fbdb98d90dea1c62b357238b16f8
Henry <hxl2000@gmail.com>
2016-08-17 16:27:54 -0600

    update

--- 01/ex4.py
+++ 01/ex4.py
@@ -1,4 +1,4 @@
-#constant define
+#constants define
 cars = 100
 space_in_a_car = 4.0
 drivers = 30
@@ -8,7 +8,7 @@ cars_driven = drivers
 carpool_capacity = cars_driven * space_in_a_car
 average_passengers_per_car = passengers / cars_driven
 
-#print defined constant on the screen
+#print defined constants on the screen
 print "There are",cars,"cars available."
 print "There are only",drivers,"drivers available."
 print "There will be",cars_not_driven,"empty cars today."
